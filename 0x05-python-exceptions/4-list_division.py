238ad87586ea56aedf0caec23c66c96dc766257b
KathokaJM <judekathoka@gmail.com>
2023-06-26 14:16:50 -0700

    0x05 Python Update

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,16 +2,6 @@
 
 
 def list_division(my_list_1, my_list_2, list_length):
-    """Divides two lists element by element.
-
-    Args:
-        my_list_1 (list): The first list.
-        my_list_2 (list): The second list.
-        list_length (int): The number of elements to divide.
-
-    Returns:
-        A new list of length list_length containing all the divisions.
-    """
     new_list = []
     for i in range(0, list_length):
         try:
@@ -28,4 +18,3 @@ def list_division(my_list_1, my_list_2, list_length):
         finally:
             new_list.append(div)
     return (new_list)
-
